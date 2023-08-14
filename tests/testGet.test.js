327dd0e00a23518fcc3d4695118b53343927136b
palhubdevops <141798474+palhubdevops@users.noreply.github.com>
2023-08-14 17:27:31 +0530

    Update testGet.test.js

--- tests/testGet.test.js
+++ tests/testGet.test.js
@@ -34,7 +34,7 @@ function createDocumentClientMock() {
 }
 
 function createHandler(mock) {
-  return proxyquire('../../handler', {
+  return proxyquire('../handler', {
     'aws-sdk': {
       DynamoDB: {
         DocumentClient: mock.DocumentClient
